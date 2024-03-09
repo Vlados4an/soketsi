94375716d1849a65f0096d9e9ac3b2b58cb9deb2
Vlados Ermalov <ssvetlaa235@gmail.com>
2024-03-09 04:32:19 +0300

    ufuf

--- src/main/java/ru/erma/socket/Server.java
+++ src/main/java/ru/erma/socket/Server.java
@@ -1,30 +1,23 @@
-// Server.java
 package ru.erma.socket;
 
 import ru.erma.socket.handler.CommandParser;
 import ru.erma.socket.handler.ServerMessageHandler;
 import ru.erma.util.Application;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Server {
-
     public static void main(String[] args) {
         try( ServerSocket server = new ServerSocket(4004) ) {
-            Application app = Application.runApp();
             CommandParser commandParser = new CommandParser();
-            ServerMessageHandler messageHandler = new ServerMessageHandler(app, commandParser);
-
+            ServerMessageHandler messageHandler = new ServerMessageHandler(Application.runApp(), commandParser);
             System.out.println("Сервер запущен!");
 
             while (true) {
                 Socket clientSocket = server.accept();
-                messageHandler.handleClient(clientSocket);
+                new Thread(() -> messageHandler.handleClient(clientSocket)).start();
             }
         } catch (IOException e) {
             System.err.println(e);
